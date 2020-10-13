3cce8a2511dde2f8506a61aca6b7e9bc6e7fa7c4
MargauxRUSSEIL <margaux.russeil.19@gmail.com>
2020-10-13 12:18:03 +0200

    colors

--- integration/src/components/VHeader.vue
+++ integration/src/components/VHeader.vue
@@ -1,7 +1,19 @@
 <template>
-    <header class="MainHeader">
-        <div class="MainHeader__container">
+    <header class="MainHeader bg-red-500">
+        <div class="MainHeader__container max-w-4xl mx-auto py-6 bg-green-300 flex">
+
             <img src="@/assets/image.svg" alt="Digits logo">
+
+            <nav class="ml-20 text-blue-dark">
+                <router-link to="/">Accueil</router-link>
+                <router-link to="/about">A propos</router-link>
+            </nav>
+
+            <div class="contact ml-auto">
+                <a href="#">+00 123 456 789</a>
+                <a href="#" class="bg-blue-button">Contact Us</a>
+            </div>
+
         </div>
     </header>
 </template>
