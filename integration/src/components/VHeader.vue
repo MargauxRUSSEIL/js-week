b5f66d48fe7fa9aff062f38e21b21b0f7a64f638
MargauxRUSSEIL <margaux.russeil.19@gmail.com>
2020-10-13 15:26:43 +0200

    Hero

--- integration/src/components/VHeader.vue
+++ integration/src/components/VHeader.vue
@@ -1,15 +1,15 @@
 <template>
     <header class="MainHeader">
-        <div class="MainHeader__container max-w-4xl mx-auto px-4 py-6 flex">
+        <div class="MainHeader__container max-w-4xl mx-auto px-4 py-4 flex items-center">
 
-            <img src="@/assets/image.svg" alt="Digits logo">
+            <img src="@/assets/logo.svg" alt="Digits logo">
 
-            <nav class="ml-20">
+            <nav class="ml-16">
                 <router-link class="link" to="/">Accueil</router-link>
                 <router-link class="link" to="/about">À propos</router-link>
             </nav>
 
-            <div class="contact ml-auto">
+            <div class="contact ml-auto flex items-center">
                 <a href="#" class="link">+00 123 456 789</a>
                 <a href="#" class="link-rounded">Contact Us</a>
             </div>
---
+++ integration/src/components/VHero.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="Hero text-center">
+        <div class="Hero__container max-w-4xl mx-auto pt-20">
+            <h1 class="title">Good design meets<br>great user experience</h1>
+            <p class="my-6 text-blue-text text-lg">For everyone, from beginners to experts</p>
+
+            <div class="buttons">
+                <a href="#" class="link-rounded mx-3">Buy Digit</a>
+                <a href="#" class="link-rounded link-rounded--white mx-3">See the Features</a>
+            </div>
+
+            <div class="illustration mt-24">
+                <img src="@/assets/illustration.svg" alt="Jeune femme qui tient un téléphone, les cheveux au vent.">
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+.Hero {
+    position: relative ;
+}
+
+.Hero::before {
+    content: '' ;
+    display: block ;
+    position: absolute ;
+    height: 80% ;
+    width: 100% ;
+    background: linear-gradient(180deg, rgba(242, 249, 255, 0.12) 0%, #F2F9FF 100%) ;
+    top: 0 ;
+    left: 0 ;
+    z-index: -1 ;
+}
+
+.title {
+    @apply text-blue-dark font-bold leading-none ;
+    font-size: 56px ;
+}
+</style>
