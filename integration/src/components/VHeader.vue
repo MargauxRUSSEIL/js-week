970a668eadb9f2f7277feaa860d570e93f02c33f
MargauxRUSSEIL <margaux.russeil.19@gmail.com>
2020-10-13 11:23:33 +0200

    logo header

---
+++ integration/src/components/VHeader.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="MainHeader">
+        <div class="MainHeader__container">
+            <img src="@/assets/image.svg" alt="Digits logo">
+        </div>
+    </header>
+</template>
