aaa7e91d8670c8c3acd0f7ad64ce2d5624a30945
MargauxRUSSEIL <margaux.russeil.19@gmail.com>
2020-10-13 12:57:56 +0200

    header

--- integration/src/components/VHeader.vue
+++ integration/src/components/VHeader.vue
@@ -1,29 +1,19 @@
 <template>
-    <header class="MainHeader bg-red-500">
-        <div class="MainHeader__container max-w-4xl mx-auto px-4 py-6 bg-red-100 flex">
+    <header class="MainHeader">
+        <div class="MainHeader__container max-w-4xl mx-auto px-4 py-6 flex">
 
             <img src="@/assets/image.svg" alt="Digits logo">
 
             <nav class="ml-20">
                 <router-link class="link" to="/">Accueil</router-link>
-                <router-link class="link" to="/about">A propos</router-link>
+                <router-link class="link" to="/about">À propos</router-link>
             </nav>
 
             <div class="contact ml-auto">
                 <a href="#" class="link">+00 123 456 789</a>
-                <a href="#" class="bg-blue-button">Contact Us</a>
+                <a href="#" class="link-rounded">Contact Us</a>
             </div>
 
         </div>
     </header>
 </template>
-
-<style lang="postcss" scoped>
-.link {
-    @apply mr-10 p-3 text-blue-dark ;
-}
-
-.link:focus, .link:hover {
-    @apply text-blue-button ;
-}
-</style>
