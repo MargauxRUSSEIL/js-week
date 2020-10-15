92988962cd92b55f27e55ae650144e73c5bcbb3d
MargauxRUSSEIL <margaux.russeil.19@gmail.com>
2020-10-15 15:14:35 +0200

    animation

--- integration/src/components/VHero.vue
+++ integration/src/components/VHero.vue
@@ -2,14 +2,14 @@
   <div class="Hero text-center">
     <div class="Hero__container container pt-20">
       <h1 ref="title" class="title text-blue-dark">Good design meets<br>great user experience</h1>
-      <p class="my-6 text-blue-text text-lg">For everyone, from beginners to experts</p>
+      <p ref="baseline" class="my-6 text-blue-text text-lg">For everyone, from beginners to experts</p>
 
-      <div class="buttons">
+      <div ref="buttons" class="buttons">
         <a href="#" class="link-rounded mx-3">Buy Digits</a>
         <a href="#" class="link-rounded link-rounded--white mx-3">See the Features</a>
       </div>
 
-      <div class="illustration mt-24">
+      <div ref="illustration" class="illustration mt-24">
         <v-illustration class="w-full h-auto"></v-illustration>
       </div>
     </div>
@@ -21,49 +21,89 @@ import gsap from 'gsap'
 import Splitting from 'splitting'
 import VIllustration from '@/components/VIllustration'
 export default {
-  components: {
-    VIllustration
-  },
-  mounted() {
-    const title = this.$refs.title
-    const chars = Splitting({ target: title, by: 'chars' })[0].chars
-    gsap.from(chars, {
-      delay: 1,
-      autoAlpha: 0,
-      y: 30,
-      stagger: 0.06,
-      // ease: 
-      duration: 0.8
-    })
-  }
+    components: {
+        VIllustration
+    },
+    mounted() {
+        const tl = gsap.timeline({
+            delay: 1
+        })
+        /**
+         * Animation du title
+         */
+        const title = this.$refs.title
+        const chars = Splitting({ target: title, by: 'chars' })[0].chars
+        tl.from(chars, {
+            delay: 1,
+            autoAlpha: 0,
+            scaleY: 0,
+            y: (i) => {
+                return (chars.length - Math.cos(i)) * Math.random() * 5
+            },
+            x: 0,
+            stagger: 0.03,
+            ease: "elastic.out(2, 0.8)",
+            duration: 0.85
+        }, 'start')
+        /**
+         * Animation de la baseline
+         */
+        tl.from(this.$refs.baseline, {
+            y: 20,
+            autoAlpha: 0,
+            ease: "expo.out",
+            duration: 0.35
+        }, 'start+=2')
+        /**
+         * Animation des boutons
+         */
+        tl.from(this.$refs.buttons.querySelectorAll('a'), {
+            y: 20,
+            autoAlpha: 0,
+            stagger: 0.1,
+            ease: "elastic.out(1, 2.8)",
+            duration: 0.8
+        })
+        /**
+         * Animation de l'illustration
+         */
+        tl.from(this.$refs.illustration, {
+            y: 50,
+            scala: 1.1,
+            autoAlpha: 0,
+            stagger: 0.1,
+            ease: "sine.out",
+            duration: 0.8
+        }, '-=1')
+    }
 }
 </script>
 
 <style lang="postcss" scoped>
 >>> .char {
-  display: inline-block;
+    display: inline-block;
 }
 .Hero {
-  position: relative;
+    position: relative;
 }
 .Hero::before {
-  content: '';
-  display: block;
-  height: 80%;
-  width: 100%;
-  background: linear-gradient(180deg, rgba(242, 249, 255, 0.12) 0%, #F2F9FF 100%);
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -1;
+    content: '';
+    display: block;
+    height: 80%;
+    width: 100%;
+    background: linear-gradient(180deg, rgba(242, 249, 255, 0.12) 0%, #F2F9FF 100%);
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
 }
 .title {
-  @apply text-blue-dark font-bold leading-none;
-  font-size: 32px;
+    @apply text-blue-dark font-bold leading-none;
+    font-size: 32px;
 }
 @screen md {
-  .title {
-    font-size: 56px;
-  }
+    .title {
+        font-size: 56px;
+    }
 }
 </style>
